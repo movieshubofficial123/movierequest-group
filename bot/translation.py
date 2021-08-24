2e2988c68cc03eeca6c853b8510e0eb26e45970b
movieshubofficial123 <85605801+movieshubofficial123@users.noreply.github.com>
2021-08-24 17:06:10 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -54,5 +54,5 @@ Use Our @movies_Hubh2 Group\n
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/NewCreate/MH-Adv-Auto-Filter-Bot-V2">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://t.me/movies_Hubh2">Click Me</a></i>
 """
